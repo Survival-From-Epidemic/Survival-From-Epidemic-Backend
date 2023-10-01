deb88e0d31436579b7f5f0d68a93b3f0504d8730
Kangmin <ehfehf0629@gmail.com>
2023-10-02 00:56:06 +0900

    Init: (#1) GlobalExceptionHandler 생성
    Controller에서 발생하는 예외를 한 곳에서 처리함으로써 코드 중복을 없애고, 책임을 분리함

---
+++ src/main/kotlin/team/sfe/server/global/error/GlobalExceptionHandler.kt
@@ -0,0 +1,50 @@
+package team.sfe.server.global.error
+
+import jakarta.validation.ConstraintViolationException
+import org.springframework.http.HttpStatus
+import org.springframework.validation.BindException
+import org.springframework.web.HttpRequestMethodNotSupportedException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException::class)
+    protected fun handleBindException(
+        e: BindException
+    ) = ErrorResponse.of(e.bindingResult)
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    protected fun handleMethodArgumentNotValidException(
+        e: MethodArgumentNotValidException
+    ) = ErrorResponse.of(e.bindingResult)
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException::class)
+    protected fun handleConstraintViolationException(
+        e: ConstraintViolationException
+    ) = ErrorResponse.of(e)
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
+    protected fun handleMethodArgumentTypeMismatchException(
+        e: MethodArgumentTypeMismatchException
+    ) = ErrorResponse.of(e)
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    protected fun handleHttpRequestMethodNotSupportedException(
+        e: HttpRequestMethodNotSupportedException
+    ) = ErrorResponse.of(e)
+
+    @ExceptionHandler(CustomException::class)
+    protected fun handleBusinessException(
+        e: CustomException
+    ) = ErrorResponse.of(e)
+}
