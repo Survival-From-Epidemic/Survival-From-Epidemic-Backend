e2c9e0843742a5cdd9670bc5229e417b7aabf015
KMS06 <jjm336949@dsm.hs.kr>
2023-10-04 11:30:20 +0900

    Stlye: ktlint 적용

--- src/main/kotlin/team/sfe/server/global/security/SecurityConfig.kt
+++ src/main/kotlin/team/sfe/server/global/security/SecurityConfig.kt
@@ -4,12 +4,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import kotlin.jvm.Throws
 
 @Configuration
 class SecurityConfig {
