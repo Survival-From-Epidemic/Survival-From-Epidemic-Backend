19c33ccd592d7dfb773fc6b41940311002c831c6
KMS06 <jjm336949@dsm.hs.kr>
2023-10-04 11:51:20 +0900

    Style: ktlint 적용

--- src/main/kotlin/team/sfe/server/global/security/SecurityConfig.kt
+++ src/main/kotlin/team/sfe/server/global/security/SecurityConfig.kt
@@ -11,7 +11,6 @@ import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig {
-
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
