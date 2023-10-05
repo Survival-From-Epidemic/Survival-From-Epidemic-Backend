30e8881c0715c1dba503ec38e23e4f04517c1728
KMS06 <jjm336949@dsm.hs.kr>
2023-10-05 16:05:18 +0900

    Init: (#12) JwtConstant 생성

---
+++ src/main/kotlin/team/sfe/server/global/security/jwt/JwtConstant.kt
@@ -0,0 +1,9 @@
+package team.sfe.server.global.security.jwt
+
+object JwtConstant {
+    const val HEADER = "Authorization"
+    const val AUTHORITY = "authority"
+    const val PREFIX = "Bearer "
+    const val ACCESS = "access"
+    const val REFRESH = "refresh"
+}
