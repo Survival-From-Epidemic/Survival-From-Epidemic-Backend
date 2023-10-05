6744ef9cac274e7f2c249b220b862998a76a53ee
KMS06 <jjm336949@dsm.hs.kr>
2023-10-05 20:27:47 +0900

    Init: (#12) JwtParser 생성

---
+++ src/main/kotlin/team/sfe/server/global/security/jwt/JwtParser.kt
@@ -0,0 +1,30 @@
+package team.sfe.server.global.security.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jws
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import org.springframework.stereotype.Component
+import team.sfe.server.global.exception.InternalServerErrorException
+import team.sfe.server.global.security.exception.ExpiredTokenException
+import team.sfe.server.global.security.exception.InvalidTokenException
+
+@Component
+class JwtParser(
+    private val jwtProperties: JwtProperties
+) {
+    private fun getClaims(token: String): Jws<Claims> {
+        return try {
+            Jwts.parser()
+                .setSigningKey(jwtProperties.secretKey)
+                .parseClaimsJws(token)
+        } catch (exception: Exception) {
+            when (exception) {
+                is ExpiredJwtException -> throw ExpiredTokenException
+                is JwtException -> throw InvalidTokenException
+                else -> throw InternalServerErrorException
+            }
+        }
+    }
+}
