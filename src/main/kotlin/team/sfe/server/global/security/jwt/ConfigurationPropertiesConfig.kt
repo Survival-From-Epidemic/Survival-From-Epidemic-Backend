aecc075f52927b5190bc9f9816509f8f0bac3d0e
KMS06 <jjm336949@dsm.hs.kr>
2023-10-05 23:25:42 +0900

    Refactor: (#12) comment 반영

---
+++ src/main/kotlin/team/sfe/server/global/security/jwt/ConfigurationPropertiesConfig.kt
@@ -0,0 +1,8 @@
+package team.sfe.server.global.security.jwt
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationPropertiesScan("team.sfe.server")
+class ConfigurationPropertiesConfig
--- src/main/kotlin/team/sfe/server/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/team/sfe/server/global/security/jwt/JwtProperties.kt
@@ -1,11 +1,9 @@
 package team.sfe.server.global.security.jwt
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties(JwtProperties::class)
 @ConfigurationProperties(prefix = "jwt")
 data class JwtProperties(
     var secretKey: String = "",
