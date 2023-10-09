63cc1cbd9ae740387c3e5648867dbf7f21e838f7
alsdl0629 <ehfehf0629@gmail.com>
2023-10-09 15:54:15 +0900

    Init: (#14) AuthDetails, AuthDetailsService 생성
    인증 성공 후 유저의 정보를 가져와 담는 역할을 함

---
+++ src/main/kotlin/team/sfe/server/global/security/auth/AuthDetails.kt
@@ -0,0 +1,26 @@
+package team.sfe.server.global.security.auth
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import team.sfe.server.domain.user.domain.type.Authority
+
+class AuthDetails(
+    private val userId: Long,
+    private val authority: Authority
+): UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
+        mutableListOf(SimpleGrantedAuthority(authority.name))
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String = userId.toString()
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
---
+++ src/main/kotlin/team/sfe/server/global/security/auth/AuthDetailsService.kt
@@ -0,0 +1,12 @@
+package team.sfe.server.global.security.auth
+
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Component
+
+@Component
+class AuthDetailsService: UserDetailsService {
+    override fun loadUserByUsername(username: String?): UserDetails {
+        TODO("Not yet implemented")
+    }
+}
