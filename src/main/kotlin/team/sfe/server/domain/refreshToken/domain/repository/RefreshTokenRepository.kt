c3fdfb0e00eef236e2ca3a7a35d224f21f836438
KMS06 <jjm336949@dsm.hs.kr>
2023-10-19 14:06:36 +0900

    Init: (#25) RefreshTokenRepository 생성

---
+++ src/main/kotlin/team/sfe/server/domain/refreshToken/domain/repository/RefreshTokenRepository.kt
@@ -0,0 +1,7 @@
+package team.sfe.server.domain.refreshToken.domain.repository
+
+import org.springframework.data.repository.CrudRepository
+import team.sfe.server.domain.refreshToken.domain.RefreshToken
+
+interface RefreshTokenRepository : CrudRepository<RefreshToken, String>{
+}
