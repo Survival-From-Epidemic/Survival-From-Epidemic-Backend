2bdc3a826427a721ed26e229bb0d532ab37466f8
KMS06 <jjm336949@dsm.hs.kr>
2023-10-20 15:38:42 +0900

    Refactor: (#25) comment 반영

--- src/main/kotlin/team/sfe/server/domain/refreshToken/domain/RefreshToken.kt
+++ src/main/kotlin/team/sfe/server/domain/refreshToken/domain/RefreshToken.kt
@@ -1,14 +1,10 @@
 package team.sfe.server.domain.refreshToken.domain
 
-import jakarta.validation.constraints.NotNull
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 
 @RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24 * 14)
 class RefreshToken(
     @Id
-    val token: String,
-
-    @field:NotNull
-    val accountId: String
+    val token: String
 )
