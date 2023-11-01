9bc71b66db02844484c0f68c37c5a6d0fe771a4c
KMS06 <jjm336949@dsm.hs.kr>
2023-11-01 11:47:47 +0900

    Refactor: (#29) comment 반영

--- src/main/kotlin/team/sfe/server/domain/user/presentation/request/UserSignInRequest.kt
+++ src/main/kotlin/team/sfe/server/domain/user/presentation/request/UserSignInRequest.kt
@@ -9,8 +9,7 @@ data class UserSignInRequest(
 
     @field:NotBlank
     @Pattern(
-        regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+-=?/])[a-zA-Z0-9~!@#$%^&*()_+-=?/]{8,20}$",
-        message = "비밀번호는는 8자에서 20자 사이입니다."
+        regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+-=?/])[a-zA-Z0-9~!@#$%^&*()_+-=?/]{8,20}$"
     )
     val password: String
 )
