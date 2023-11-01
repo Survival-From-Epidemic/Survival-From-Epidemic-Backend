663b57459a2de604472930a94f4fea6cc3402d02
KMS06 <jjm336949@dsm.hs.kr>
2023-11-01 22:12:50 +0900

    Refactor: (#29) comment 반영

--- src/main/kotlin/team/sfe/server/domain/user/service/UserSignInService.kt
+++ src/main/kotlin/team/sfe/server/domain/user/service/UserSignInService.kt
@@ -3,22 +3,21 @@ package team.sfe.server.domain.user.service
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import team.sfe.server.domain.user.domain.repository.UserRepository
 import team.sfe.server.domain.user.exception.PasswordMisMatchException
-import team.sfe.server.domain.user.exception.UserNotFoundException
+import team.sfe.server.domain.user.facade.UserFacade
 import team.sfe.server.domain.user.presentation.request.UserSignInRequest
 import team.sfe.server.domain.user.presentation.response.TokenResponse
 import team.sfe.server.global.security.jwt.JwtProvider
 
 @Service
 class UserSignInService(
-    private val userRepository: UserRepository,
+    private val userFacade: UserFacade,
     private val jwtProvider: JwtProvider,
     private val passwordEncoder: PasswordEncoder
 ) {
     @Transactional
     fun execute(request: UserSignInRequest): TokenResponse {
-        val user = userRepository.findByAccountId(request.accountId) ?: throw UserNotFoundException
+        val user = userFacade.getUserByAccountId(request.accountId)
 
         if (!passwordEncoder.matches(request.password, user.password)) {
             throw PasswordMisMatchException
