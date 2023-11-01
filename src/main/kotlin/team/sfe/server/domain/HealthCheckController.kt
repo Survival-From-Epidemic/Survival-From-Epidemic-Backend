a26d865658571641d2cbebedd16b40e033d7d163
alsdl0629 <ehfehf0629@gmail.com>
2023-11-01 11:32:41 +0900

    Chore: (#31) health check url

--- src/main/kotlin/team/sfe/server/domain/HealthCheckController.kt
+++ src/main/kotlin/team/sfe/server/domain/HealthCheckController.kt
@@ -1,8 +1,10 @@
 package team.sfe.server.domain
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@RequestMapping("/health-check")
 @RestController
 class HealthCheckController {
 
