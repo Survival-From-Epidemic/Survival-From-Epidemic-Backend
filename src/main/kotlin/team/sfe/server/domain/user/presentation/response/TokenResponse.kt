9c3c5819d9c8681251c81a9dafaabbd98cad14b3
KMS06 <jjm336949@dsm.hs.kr>
2023-11-01 11:48:21 +0900

    Refactor: (#29) comment 반영

--- src/main/kotlin/team/sfe/server/domain/user/presentation/response/TokenResponse.kt
+++ src/main/kotlin/team/sfe/server/domain/user/presentation/response/TokenResponse.kt
@@ -1,10 +1,12 @@
 package team.sfe.server.domain.user.presentation.response
 
-import team.sfe.server.domain.refreshToken.domain.RefreshToken
 import team.sfe.server.domain.user.domain.type.Authority
+import java.time.LocalDateTime
 
 data class TokenResponse(
     val accessToken: String,
-    val refreshToken: RefreshToken,
+    val refreshToken: String,
+    val accessTokenExpiredAt: LocalDateTime,
+    val refreshTokenExpiredAt: LocalDateTime,
     val authority: Authority
 )
