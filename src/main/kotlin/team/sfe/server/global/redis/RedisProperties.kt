480e76466fb87e5cb14ecc268e211e124b204ec1
Kangmin <ehfehf0629@gmail.com>
2023-11-08 12:12:32 +0900

    Fix: (main) redis property 수정

--- src/main/kotlin/team/sfe/server/global/redis/RedisProperties.kt
+++ src/main/kotlin/team/sfe/server/global/redis/RedisProperties.kt
@@ -2,7 +2,7 @@ package team.sfe.server.global.redis
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "spring.redis")
+@ConfigurationProperties(prefix = "spring.redis.data")
 data class RedisProperties(
     val host: String,
     val port: Int
