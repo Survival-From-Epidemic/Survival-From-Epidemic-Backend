28929c113ffe1abf333d90eb519d5fff64803aa2
Kangmin <ehfehf0629@gmail.com>
2023-11-08 09:02:27 +0900

    Chore: (main) Controller 변경

---
+++ src/main/kotlin/team/sfe/server/domain/auth/domain/presentation/AuthController.kt
@@ -0,0 +1,38 @@
+package team.sfe.server.domain.auth.domain.presentation
+
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import team.sfe.server.domain.auth.service.TokenRefreshService
+import team.sfe.server.domain.auth.service.UserSignInService
+import team.sfe.server.domain.user.presentation.request.UserSignInRequest
+import team.sfe.server.domain.user.presentation.response.TokenResponse
+
+@RequestMapping("/auth")
+@RestController
+class AuthController(
+    private val userSignInService: UserSignInService,
+    private val tokenRefreshService: TokenRefreshService
+) {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/tokens")
+    fun signIn(
+        @RequestBody @Valid
+        request: UserSignInRequest
+    ): TokenResponse {
+        return userSignInService.execute(request)
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/tokens")
+    fun reissue(@RequestHeader("Refresh-Token") refreshToken: String): TokenResponse {
+        return tokenRefreshService.execute(refreshToken)
+    }
+}
--- src/main/kotlin/team/sfe/server/domain/user/presentation/UserController.kt
+++ src/main/kotlin/team/sfe/server/domain/user/presentation/UserController.kt
@@ -2,16 +2,11 @@ package team.sfe.server.domain.user.presentation
 
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import team.sfe.server.domain.auth.service.TokenRefreshService
-import team.sfe.server.domain.auth.service.UserSignInService
-import team.sfe.server.domain.user.presentation.request.UserSignInRequest
 import team.sfe.server.domain.user.presentation.request.UserSignUpRequest
 import team.sfe.server.domain.user.presentation.response.TokenResponse
 import team.sfe.server.domain.user.service.UserSignUpService
@@ -19,9 +14,7 @@ import team.sfe.server.domain.user.service.UserSignUpService
 @RequestMapping("/users")
 @RestController
 class UserController(
-    private val userSignUpService: UserSignUpService,
-    private val userSignInService: UserSignInService,
-    private val tokenRefreshService: TokenRefreshService
+    private val userSignUpService: UserSignUpService
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -31,20 +24,5 @@ class UserController(
         request: UserSignUpRequest
     ): TokenResponse {
         return userSignUpService.execute(request)
-    }
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/tokens")
-    fun signIn(
-        @RequestBody @Valid
-        request: UserSignInRequest
-    ): TokenResponse {
-        return userSignInService.execute(request)
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PatchMapping("/tokens")
-    fun reissue(@RequestHeader("Refresh-Token") refreshToken: String): TokenResponse {
-        return tokenRefreshService.execute(refreshToken)
     }
 }
