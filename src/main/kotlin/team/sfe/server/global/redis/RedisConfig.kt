77fde2534d35eebfdac643796f3a674446df0d2f
Kangmin <ehfehf0629@gmail.com>
2023-11-08 22:41:32 +0900

    Chore: (main) Redis password 적용

--- src/main/kotlin/team/sfe/server/global/redis/RedisConfig.kt
+++ src/main/kotlin/team/sfe/server/global/redis/RedisConfig.kt
@@ -3,6 +3,8 @@ package team.sfe.server.global.redis
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisKeyValueAdapter
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
@@ -12,8 +14,15 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 class RedisConfig(
     private val redisProperties: RedisProperties
 ) {
+
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory(redisProperties.host, redisProperties.port)
+        val redisConfiguration = RedisStandaloneConfiguration().apply {
+            hostName = redisProperties.host
+            port = redisProperties.port
+            password = RedisPassword.of(redisProperties.password)
+        }
+
+        return LettuceConnectionFactory(redisConfiguration)
     }
 }
--- src/main/kotlin/team/sfe/server/global/redis/RedisProperties.kt
+++ src/main/kotlin/team/sfe/server/global/redis/RedisProperties.kt
@@ -2,8 +2,9 @@ package team.sfe.server.global.redis
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "spring.redis.data")
+@ConfigurationProperties(prefix = "spring.redis")
 data class RedisProperties(
     val host: String,
-    val port: Int
+    val port: Int,
+    val password: String,
 )
