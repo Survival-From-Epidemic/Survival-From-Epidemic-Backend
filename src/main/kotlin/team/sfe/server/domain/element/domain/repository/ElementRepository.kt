af757013946d5c42c59511b1afd4659f224244cf
KMS06 <jjm336949@dsm.hs.kr>
2023-11-09 11:29:57 +0900

    Init: (#33) comment 반영

---
+++ src/main/kotlin/team/sfe/server/domain/element/domain/repository/ElementRepository.kt
@@ -0,0 +1,6 @@
+package team.sfe.server.domain.element.domain.repository
+
+import org.springframework.data.repository.CrudRepository
+import team.sfe.server.domain.element.domain.Element
+
+interface ElementRepository: CrudRepository<Element, Long>
