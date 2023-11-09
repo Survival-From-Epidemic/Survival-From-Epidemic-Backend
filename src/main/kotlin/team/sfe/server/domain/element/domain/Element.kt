ca74b66e7873d8e052457a37a6afbf0e7f23d3aa
KMS06 <jjm336949@dsm.hs.kr>
2023-11-09 10:51:04 +0900

    Init: (#33) Element 생성

---
+++ src/main/kotlin/team/sfe/server/domain/element/domain/Element.kt
@@ -0,0 +1,21 @@
+package team.sfe.server.domain.element.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.validation.constraints.NotNull
+import team.sfe.server.domain.element.domain.type.Type
+import team.sfe.server.global.entity.BaseIdEntity
+
+@Entity
+class Element(
+    override val id: Long = 0L,
+
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(20)")
+    val name: String,
+
+    @field:NotNull
+    @Column(columnDefinition = "CHAR(3)")
+    val type: Type
+
+) : BaseIdEntity(id)
