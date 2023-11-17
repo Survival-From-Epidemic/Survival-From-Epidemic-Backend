aa4e7f057fe4ce88ced4d6e02cf8f77feafe9227
Kangmin <ehfehf0629@gmail.com>
2023-11-17 10:25:16 +0900

    Init: (#39) GetProgressResponse

---
+++ src/main/kotlin/team/sfe/server/domain/progress/presentation/dto/GetProgressResponse.kt
@@ -0,0 +1,33 @@
+package team.sfe.server.domain.progress.presentation.dto
+
+import team.sfe.server.domain.progress.domain.vo.QueryProgressVO
+
+data class GetProgressResponse(
+    val disease: Disease,
+    val person: Person,
+    val persons: List<PersonData>,
+    val diseaseEnabled: Boolean,
+    val pcrEnabled: Boolean,
+    val kitEnabled: Boolean,
+    val kitChance: Int,
+    val vaccineResearch: Boolean,
+    val vaccineEnded: Boolean
+) {
+
+    companion object {
+
+        fun of(vo: QueryProgressVO) = GetProgressResponse(
+            disease = vo.diseaseEntity.toDiseaseDto(),
+            person = vo.personEntity.toPersonDto(),
+            persons = vo.personDataEntity.map {
+                it.toPersonDataDto()
+            },
+            diseaseEnabled = vo.gameInfoEntity.diseaseEnabled,
+            pcrEnabled = vo.gameInfoEntity.pcrEnabled,
+            kitEnabled = vo.gameInfoEntity.kitEnabled,
+            kitChance = vo.gameInfoEntity.kitChance,
+            vaccineResearch = vo.gameInfoEntity.vaccineResearch,
+            vaccineEnded = vo.gameInfoEntity.vaccineEnded
+        )
+    }
+}
