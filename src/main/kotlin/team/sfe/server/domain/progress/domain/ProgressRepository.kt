8a6f60e8d61eecbb97571f5052b8e0d9efd56481
Kangmin <ehfehf0629@gmail.com>
2023-11-23 10:27:24 +0900

    Feat: (develop) 진행상황 수정 API

--- src/main/kotlin/team/sfe/server/domain/progress/domain/ProgressRepository.kt
+++ src/main/kotlin/team/sfe/server/domain/progress/domain/ProgressRepository.kt
@@ -4,10 +4,8 @@ import com.querydsl.core.group.GroupBy.groupBy
 import com.querydsl.core.group.GroupBy.list
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
-import team.sfe.server.domain.progress.domain.QDiseaseEntity.diseaseEntity
 import team.sfe.server.domain.progress.domain.QGameInfoEntity.gameInfoEntity
 import team.sfe.server.domain.progress.domain.QPersonDataEntity.personDataEntity
-import team.sfe.server.domain.progress.domain.QPersonEntity.personEntity
 import team.sfe.server.domain.progress.domain.vo.QQueryProgressVO
 import team.sfe.server.domain.progress.domain.vo.QueryProgressVO
 import team.sfe.server.domain.user.domain.QUserEntity.userEntity
@@ -20,18 +18,16 @@ class ProgressRepository(
     fun queryProgress(userId: Long): QueryProgressVO? {
         return jpaQueryFactory
             .selectFrom(userEntity)
-            .join(diseaseEntity.userEntity, userEntity)
             .join(gameInfoEntity.userEntity, userEntity)
+            .on(gameInfoEntity.userEntity.id.eq(userId))
             .join(personDataEntity.userEntity, userEntity)
-            .join(personEntity.userEntity, userEntity)
+            .on(personDataEntity.userEntity.id.eq(userId))
             .where(userEntity.id.eq(userId))
             .transform(
                 groupBy(userEntity.id).list(
                     QQueryProgressVO(
-                        diseaseEntity,
                         gameInfoEntity,
-                        list(personDataEntity),
-                        personEntity
+                        list(personDataEntity)
                     )
                 )
             )[0]
