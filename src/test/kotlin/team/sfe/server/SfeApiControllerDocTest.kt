3371777120aa8a5e53b377174e5c9d8052afb09c
Kangmin <ehfehf0629@gmail.com>
2023-11-28 21:36:16 +0900

    Chore: (#42) 테스트 코드 주석 처리

--- src/test/kotlin/team/sfe/server/SfeApiControllerDocTest.kt
+++ src/test/kotlin/team/sfe/server/SfeApiControllerDocTest.kt
@@ -1,22 +1,22 @@
-package team.sfe.server
-
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
-import org.springframework.restdocs.RestDocumentationContextProvider
-import org.springframework.restdocs.RestDocumentationExtension
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
-
+//package team.sfe.server
+//
+//import org.junit.jupiter.api.BeforeEach
+//import org.junit.jupiter.api.Test
+//import org.junit.jupiter.api.extension.ExtendWith
+//import org.springframework.boot.test.context.SpringBootTest
+//import org.springframework.http.MediaType
+//import org.springframework.restdocs.RestDocumentationContextProvider
+//import org.springframework.restdocs.RestDocumentationExtension
+//import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+//import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+//import org.springframework.test.web.servlet.MockMvc
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+//import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+//import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders
+//import org.springframework.web.context.WebApplicationContext
+//
 //@ExtendWith(RestDocumentationExtension::class)
 //@SpringBootTest
 //class SfeApiControllerDocTest {
