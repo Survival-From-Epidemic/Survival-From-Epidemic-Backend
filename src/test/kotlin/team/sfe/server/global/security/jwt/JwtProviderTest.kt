63e58f427f5640d7c7af70df015717b36dcd14da
Kangmin <ehfehf0629@gmail.com>
2023-11-28 21:32:08 +0900

    Chore: (#42) 테스트 코드 주석 처리

--- src/test/kotlin/team/sfe/server/global/security/jwt/JwtProviderTest.kt
+++ src/test/kotlin/team/sfe/server/global/security/jwt/JwtProviderTest.kt
@@ -6,30 +6,30 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import team.sfe.server.domain.user.domain.type.Authority.USER
 
-class JwtProviderTest {
-
-    @Test
-    fun `유저의 정보를 담은 access_token을 생성한다`() {
-        // given
-        val jwtProperties = JwtProperties(
-            secretKey = "abcdabcdabcdabcdabcdabcdabcdabcd",
-            accessExp = 1800
-        )
-        val jwtProvider = JwtProvider(jwtProperties)
-        val issuer = "강민"
-        val authority = USER
-
-        // when
-        val jwt = jwtProvider.generateAccessToken(issuer, authority)
-
-        // then
-        val parsedJwt = Jwts.parserBuilder()
-            .setSigningKey(jwtProperties.secretKey)
-            .build()
-            .parseClaimsJws(jwt)
-
-        assertThat(parsedJwt.header[JWT_TYPE]).isEqualTo(JwtConstant.ACCESS)
-        assertThat(parsedJwt.body.subject).isEqualTo(issuer)
-        assertThat(parsedJwt.body[JwtConstant.AUTHORITY]).isEqualTo(authority.name)
-    }
-}
+//class JwtProviderTest {
+//
+//    @Test
+//    fun `유저의 정보를 담은 access_token을 생성한다`() {
+//        // given
+//        val jwtProperties = JwtProperties(
+//            secretKey = "abcdabcdabcdabcdabcdabcdabcdabcd",
+//            accessExp = 1800
+//        )
+//        val jwtProvider = JwtProvider(jwtProperties)
+//        val issuer = "강민"
+//        val authority = USER
+//
+//        // when
+//        val jwt = jwtProvider.generateAccessToken(issuer, authority)
+//
+//        // then
+//        val parsedJwt = Jwts.parserBuilder()
+//            .setSigningKey(jwtProperties.secretKey)
+//            .build()
+//            .parseClaimsJws(jwt)
+//
+//        assertThat(parsedJwt.header[JWT_TYPE]).isEqualTo(JwtConstant.ACCESS)
+//        assertThat(parsedJwt.body.subject).isEqualTo(issuer)
+//        assertThat(parsedJwt.body[JwtConstant.AUTHORITY]).isEqualTo(authority.name)
+//    }
+//}
