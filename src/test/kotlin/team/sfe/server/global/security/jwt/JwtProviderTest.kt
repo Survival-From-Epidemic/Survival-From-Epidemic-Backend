c8491e6bd54540f829888e2b5ba16e5641850b24
Kangmin <ehfehf0629@gmail.com>
2023-12-03 13:15:35 +0900

    Fix: (#42) conflict 해결

--- src/test/kotlin/team/sfe/server/global/security/jwt/JwtProviderTest.kt
+++ src/test/kotlin/team/sfe/server/global/security/jwt/JwtProviderTest.kt
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package team.sfe.server.global.security.jwt
 
 import io.jsonwebtoken.Header.JWT_TYPE
@@ -84,40 +83,3 @@ class JwtProviderTest(
         .build()
         .parseClaimsJws(token)
 }
-=======
-// package team.sfe.server.global.security.jwt
-//
-// import io.jsonwebtoken.Header.JWT_TYPE
-// import io.jsonwebtoken.Jwts
-// import org.assertj.core.api.Assertions.assertThat
-// import org.junit.jupiter.api.Test
-// import team.sfe.server.domain.user.domain.type.Authority.USER
-//
-// class JwtProviderTest {
-//
-//    @Test
-//    fun `유저의 정보를 담은 access_token을 생성한다`() {
-//        // given
-//        val jwtProperties = JwtProperties(
-//            secretKey = "abcdabcdabcdabcdabcdabcdabcdabcd",
-//            accessExp = 1800
-//        )
-//        val jwtProvider = JwtProvider(jwtProperties)
-//        val issuer = "강민"
-//        val authority = USER
-//
-//        // when
-//        val jwt = jwtProvider.generateAccessToken(issuer, authority)
-//
-//        // then
-//        val parsedJwt = Jwts.parserBuilder()
-//            .setSigningKey(jwtProperties.secretKey)
-//            .build()
-//            .parseClaimsJws(jwt)
-//
-//        assertThat(parsedJwt.header[JWT_TYPE]).isEqualTo(JwtConstant.ACCESS)
-//        assertThat(parsedJwt.body.subject).isEqualTo(issuer)
-//        assertThat(parsedJwt.body[JwtConstant.AUTHORITY]).isEqualTo(authority.name)
-//    }
-// }
->>>>>>> d1631bf2aef91d2e4be6611783ae0dc39f4d6c55
